f5c21c54c8343bc78d0c24de0a602999adb3f881
TheHeadpatCat <None.>
2025-04-24 20:02:25 +0200

    crash fix on zone swap

--- WoL-Plugin/Game/Rules/PVE/FailMechanic.cs
+++ WoL-Plugin/Game/Rules/PVE/FailMechanic.cs
@@ -37,35 +37,44 @@ namespace WoLightning.WoL_Plugin.Game.Rules.PVE
 
         private void Check(IFramework framework)
         {
-            if (Player == null) { Player = Plugin.ClientState.LocalPlayer; return; }
-
-            var Statuses = Player.StatusList;
-            if (Statuses == null || Statuses.Length == 0) { lastVulnUpStacks = 0; lastDamageDownStacks = 0; return; }
-
-            bool foundVuln = false, foundDamage = false;
-            foreach (var Status in Statuses)
+            try
             {
-                if (Status == null) continue;
-                // Yes. We have to check for the IconId.
-                // The StatusId is different for different expansions, while the Name is different through languages.
-                if (Status.GameData.Value.Icon >= 17101 && Status.GameData.Value.Icon <= 17116) // Vuln Up
-                {
-                    var amount = Status.Param;
-                    if (amount > lastVulnUpStacks) Trigger("You have failed a Mechanic!");
-                    lastVulnUpStacks = amount;
-                    foundVuln = true;
-                }
+                Player = Plugin.ClientState.LocalPlayer;
+                if (Player == null) { return; }
 
-                if (Status.GameData.Value.Icon >= 18441 && Status.GameData.Value.Icon <= 18456) // Damage Down
-                {
-                    var amount = Status.Param;
-                    if (amount > lastDamageDownStacks) Trigger("You have failed a Mechanic!");
-                    lastDamageDownStacks = amount;
-                    foundDamage = true;
-                }
+                var Statuses = Player.StatusList;
+                if (Statuses == null || Statuses.Length == 0) { lastVulnUpStacks = 0; lastDamageDownStacks = 0; return; }
 
-                if (!foundVuln) lastVulnUpStacks = 0;
-                if (!foundDamage) lastDamageDownStacks = 0;
+                bool foundVuln = false, foundDamage = false;
+                foreach (var Status in Statuses)
+                {
+                    if (Status == null) continue;
+
+                    // Yes. We have to check for the IconId.
+                    // The StatusId is different for different expansions, while the Name is different through languages.
+                    if (Status.GameData.Value.Icon >= 17101 && Status.GameData.Value.Icon <= 17116) // Vuln Up
+                    {
+                        var amount = Status.Param;
+                        if (amount > lastVulnUpStacks) Trigger("You have failed a Mechanic!");
+                        lastVulnUpStacks = amount;
+                        foundVuln = true;
+                    }
+
+                    if (Status.GameData.Value.Icon >= 18441 && Status.GameData.Value.Icon <= 18456) // Damage Down
+                    {
+                        var amount = Status.Param;
+                        if (amount > lastDamageDownStacks) Trigger("You have failed a Mechanic!");
+                        lastDamageDownStacks = amount;
+                        foundDamage = true;
+                    }
+
+                    if (!foundVuln) lastVulnUpStacks = 0;
+                    if (!foundDamage) lastDamageDownStacks = 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Plugin.Error(ex.StackTrace);
             }
         }
 
