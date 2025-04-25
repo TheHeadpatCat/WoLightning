e87eca6a5819001e0141794d72aaccb2f39eb4bb
TheHeadpatCat <None.>
2025-04-25 14:30:25 +0200

    ui footer change

--- WoL-Plugin/Windows/ConfigWindow.cs
+++ WoL-Plugin/Windows/ConfigWindow.cs
@@ -39,7 +39,7 @@ public class ConfigWindow : Window, IDisposable
 
         SizeConstraints = new WindowSizeConstraints
         {
-            MinimumSize = new Vector2(580, 660),
+            MinimumSize = new Vector2(580, 690),
             MaximumSize = new Vector2(2000, 2000)
         };
         Plugin = plugin;
@@ -136,11 +136,13 @@ public class ConfigWindow : Window, IDisposable
         }
 
 
-        ImGui.SetCursorPosY(ImGui.GetWindowHeight() - 40);
-        ImGui.Separator();
+        ImGui.SetNextWindowPos(new Vector2(ImGui.GetWindowPos().X,ImGui.GetWindowPos().Y + ImGui.GetWindowHeight() - 40));
+        ImGui.SetNextWindowSize(new Vector2(ImGui.GetWindowWidth(), 40));
+        ImGui.PushStyleColor(ImGuiCol.ChildBg, new Vector4(0.05f, 0.05f, 0.05f, 0.95f));
+        ImGui.Begin("##DiscordFooter", ImGuiWindowFlags.NoDocking | ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoSavedSettings | ImGuiWindowFlags.ChildWindow);
         ImGui.Text("Found a issue or got a feature request?");
         ImGui.SameLine();
-        if (ImGui.Button("Join the Discord",new Vector2(130,25)))
+        if (ImGui.Button("Join the Discord", new Vector2(130, 25)))
         {
             try
             {
@@ -150,8 +152,11 @@ public class ConfigWindow : Window, IDisposable
                     UseShellExecute = true
                 });
             }
-            catch(Exception e) { Plugin.Log(e); }
+            catch (Exception e) { Plugin.Log(e); }
         }
+        ImGui.End();
+        ImGui.PopStyleColor();
+       
     }
 
     private Vector2 ImVec2(object x, object value)
