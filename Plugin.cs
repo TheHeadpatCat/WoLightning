cb3dc938f4c2cf5dfa9692247d10efd78d733763
TheHeadpatCat <None.>
2025-06-19 20:40:37 +0200

    bump version

--- Plugin.cs
+++ Plugin.cs
@@ -37,8 +37,8 @@ public sealed class Plugin : IDalamudPlugin
     private const string OpenConfigFolder = "/wolfolder";
     private const string OpenShockRemote = "/wolremote";
 
-    public const int currentVersion = 549;
-    public const String currentVersionString = "0.5.4.9";
+    public const int currentVersion = 550;
+    public const String currentVersionString = "0.5.5.0";
     public const int configurationVersion = 501;
     public const string randomKey = "Currently Unused";
 
