ca90c76f0033e9b9b1c3a534db8881fb46e04e62
TheHeadpatCat <None.>
2025-06-30 19:25:33 +0200

    disallow Tell_Incoming as it created false positives

--- WoL-Plugin/Game/Rules/Social/HearWord.cs
+++ WoL-Plugin/Game/Rules/Social/HearWord.cs
@@ -70,7 +70,7 @@ namespace WoLightning.WoL_Plugin.Game.Rules.Social
 
                 Logger.Log(4, sender);
 
-                if ((int)type <= 107)
+                if ((int)type <= 107 && (int)type != 13) // Allow all possible social channels, EXCEPT Tell_Incoming
                 {
                     string message = StringSanitizer.LetterOrDigit(messageE.ToString());
                     foreach (var TriggerWord in TriggerWords)
