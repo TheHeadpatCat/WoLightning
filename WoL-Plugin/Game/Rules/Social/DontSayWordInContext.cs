41cbd2ea59c3ff6aec391950df263be4563bf885
TheHeadpatCat <None.>
2025-12-20 00:42:38 +0100

    update dalamud

---
+++ WoL-Plugin/Game/Rules/Social/DontSayWordInContext.cs
@@ -0,0 +1,300 @@
+﻿using Dalamud.Bindings.ImGui;
+using Dalamud.Game.Text;
+using Dalamud.Game.Text.SeStringHandling;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using WoLightning.Util;
+using WoLightning.Util.Types;
+using WoLightning.WoL_Plugin.Util;
+using WoLightning.WoL_Plugin.Util.Types;
+
+namespace WoLightning.WoL_Plugin.Game.Rules.Social
+{
+    public class DontSayWordInContext : RuleBase
+    {
+        public override string Name { get; } = "Forget to say a Word in a Context";
+
+        public override string Description { get; } = "Triggers whenever you do not say a word in a specific sentence context.";
+        override public string Hint { get; } = "Once enabled, you can set the Words in the \"Word List\" Tab!";
+
+        public override RuleCategory Category { get; } = RuleCategory.Social;
+
+        public List<SpecificWordWithContext> ContextualWords { get; set; } = new();
+        public List<XivChatType> Chats { get; set; } = new();
+        [JsonIgnore] private bool isChatLimiterOpen = false;
+
+        override public bool hasAdvancedOptions { get; } = true;
+        public override bool hasExtraButton { get; } = true;
+
+        [JsonIgnore] string Input = string.Empty;
+        [JsonIgnore] int Index = -1;
+        [JsonIgnore] String SelectedWord = string.Empty;
+        [JsonIgnore] int[] Settings = new int[3];
+
+        [JsonIgnore] bool Punctuation = false;
+        [JsonIgnore] bool ProperCase = false;
+
+
+        [JsonConstructor]
+        public DontSayWordInContext() { }
+        public DontSayWordInContext(Plugin plugin) : base(plugin)
+        {
+
+        }
+        override public void Start()
+        {
+            if (IsRunning) return;
+            IsRunning = true;
+            Service.ChatGui.ChatMessage += Check;
+        }
+
+        override public void Stop()
+        {
+            if (!IsRunning) return;
+            IsRunning = false;
+            Service.ChatGui.ChatMessage -= Check;
+        }
+        private void Check(XivChatType type, int timestamp, ref SeString senderE, ref SeString messageE, ref bool isHandled)
+        {
+            try
+            {
+                if (ContextualWords.Count == 0) return;
+
+                //check for chat type limitation
+                if (Chats.Count >= 1 && !Chats.Contains(type)) return;
+
+                // Check if its actually a emote. We don't want to trigger on emotes.
+                if (type == XivChatType.StandardEmote || type == XivChatType.CustomEmote) return;
+
+                Player? sender = null;
+                foreach (var payload in senderE.Payloads)
+                {
+                    if (payload.Type == PayloadType.Player) sender = new(payload);
+                }
+
+                Logger.Log(3, $"{Name} | Message from: " + senderE.TextValue + " comparing against " + Plugin.LocalPlayer.Name + " type: " + type.ToString());
+
+                string senderClean = StringSanitizer.LetterOrDigit(senderE.TextValue);
+                if (sender == null && senderClean == Plugin.LocalPlayer.Name) sender = Plugin.LocalPlayer; // If there is no player payload, check if names match atleast.
+
+                if (sender != Plugin.LocalPlayer && type == XivChatType.TellOutgoing) sender = Plugin.LocalPlayer;
+
+                if (sender == null || sender != Plugin.LocalPlayer) return;
+
+                Logger.Log(3, $"{Name} | Comparing sender " + sender + " against " + Plugin.LocalPlayer + " is same player?: " + sender.Equals(Plugin.LocalPlayer));
+
+                if ((int)type <= 107 &&
+                    (sender.Equals(Plugin.LocalPlayer)
+                    || type == XivChatType.TellOutgoing)) // Tell is a very weird channel. It considers only the target player and gives us no reference of the actual sender.
+                {
+                    string message = StringSanitizer.LetterOrDigit(messageE.ToString());
+                    bool found = false;
+
+                    foreach (var contextualWord in ContextualWords) // Go through every banned word the user put in.
+                    {
+
+                        Logger.Log(3, $"{Name} | Comparing " + message + " against [" + contextualWord + "] which is " + contextualWord.MatchesContext(message));
+
+                        if (contextualWord.MatchesContext(message)) // Now, with both parts. Check each said word, against all banned words. If any of them match, Trigger the Rule and end the Logic.
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) Trigger($"You forgot to say a Enforced Word!", sender);
+                }
+            }
+            catch (Exception e) { Logger.Error(Name + " Check() failed."); Logger.Error(e.Message); if(e.StackTrace != null) Logger.Error(e.StackTrace); }
+        }
+
+        public override void DrawExtraButton()
+        {
+            ImGui.SameLine();
+            if (ImGui.Button("Open Chat Limiter##DontSayWordOpenButton"))
+            {
+                isChatLimiterOpen = true;
+                ImGui.OpenPopup("Chat Limiter##DontSayWordChatLimiter");
+            }
+
+            Vector2 center = ImGui.GetMainViewport().GetCenter();
+            ImGui.SetNextWindowPos(center, ImGuiCond.Appearing, new Vector2(0.5f, 0.5f));
+            ImGui.SetNextWindowSize(new Vector2(640, 555));
+
+            if (ImGui.BeginPopupModal("Chat Limiter##DontSayWordChatLimiter", ref isChatLimiterOpen,
+                ImGuiWindowFlags.Modal | ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.Popup))
+            {
+
+                ImGui.Text("This Rule will only work on the selected Channels,\nunless no Channel is selected.");
+                ImGui.Separator();
+
+                var i = 0;
+                foreach (var e in ChatType.GetOrderedChannels()) // Old Code from @lexiconmage
+                {
+                    // See if it is already enabled by default
+                    var enabled = Chats.Contains((XivChatType)ChatType.GetXivChatTypeFromChatType(e)!);
+                    // Create a new line after every 4 columns
+                    if (i != 0 && (i == 4 || i == 7 || i == 11 || i == 15 || i == 19 || i == 23))
+                    {
+                        ImGui.NewLine();
+                        //i = 0;
+                    }
+                    // Move to the next row if it is LS1 or CWLS1
+                    if (e is ChatType.ChatTypes.LS1 or ChatType.ChatTypes.CWL1)
+                        ImGui.Separator();
+
+                    if (ImGui.Checkbox($"{e}", ref enabled))
+                    {
+                        XivChatType type = (XivChatType)ChatType.GetXivChatTypeFromChatType(e)!;
+
+                        // See If the UIHelpers.Checkbox is clicked, If not, add to the list of enabled channels, otherwise, remove it.
+                        if (enabled) Chats.Add(type);
+                        else Chats.Remove(type);
+
+                        if (type == XivChatType.TellIncoming) // Tell is split into 2 parts for some reason, so add the second part as well.
+                        {
+                            if (enabled) Chats.Add(XivChatType.TellOutgoing);
+                            else Chats.Remove(XivChatType.TellOutgoing);
+                        }
+
+                    }
+
+                    ImGui.SameLine();
+                    i++;
+                }
+                ImGui.NewLine();
+
+                if (ImGui.Button("Apply##DontSayWordApply", new Vector2(ImGui.GetWindowSize().X / 2 - 10, 25)))
+                {
+                    isChatLimiterOpen = false;
+                    Plugin.Configuration.Save();
+                    ImGui.CloseCurrentPopup();
+                }
+                ImGui.SameLine();
+                ImGui.PushItemWidth(ImGui.GetWindowSize().X / 2);
+                if (ImGui.Button("Reset All##DontSayWordReset", new Vector2(ImGui.GetWindowSize().X / 2 - 10, 25)))
+                {
+                    Chats.Clear();
+                }
+
+                ImGui.EndPopup();
+            }
+
+
+        }
+        public override void DrawAdvancedOptions()
+        {
+            ImGui.Text("Not saying atleast one Word from this list, will trigger the \"Forget to say Enforced Word\" Rule!");
+            ImGui.TextColored(new Vector4(0.66f,0.66f,0.66f,0.80f),"Note: Despite saying \"Word\", sentences are also supported.");
+
+            ImGui.SetNextItemWidth(ImGui.GetWindowWidth() - 230);
+            if (ImGui.InputTextWithHint("##EnforcedWordInput", "Click on a entry to edit it.", ref Input, 48))
+            {
+                if (Index != -1) // Get rid of the old settings, otherwise we build connections between two items
+                {
+                    int[] copyArray = new int[3];
+                    Settings.CopyTo(copyArray, 0);
+                    Settings = copyArray;
+                }
+                Index = -1;
+            }
+            ImGui.Checkbox("Punctuation", ref Punctuation);
+            ImGui.SameLine();
+            ImGui.TextDisabled("(?)");
+            if (ImGui.IsItemHovered())
+            {
+                ImGui.SetTooltip("If this is enabled, the Word has to be properly written out on its own." +
+                "\nIf the word is \"Master\" then writing \"i like my master\" is accepted, while \"ilikemymaster\" isn't.");
+            }
+            ImGui.SameLine();
+            ImGui.Checkbox("Proper Case", ref ProperCase);
+            ImGui.SameLine();
+            ImGui.TextDisabled("(?)");
+            if (ImGui.IsItemHovered())
+            {
+                ImGui.SetTooltip("If this is enabled, the Word has to properly match the Case." +
+                "\nIf the word is \"Collar\" then writing \"i have a Collar\" is accepted, while \"i have a collar\" isn't.");
+            }
+
+            ImGui.Separator();
+
+            ImGui.Spacing();
+
+            if (ImGui.Button("Add/Edit Word##EnforcedWordAdd", new Vector2(ImGui.GetWindowWidth() / 2 - 8, 25)))
+            {
+
+                SpecificWord? target = null;
+                foreach (SpecificWord EnforcedWord in EnforcedWords)
+                {
+                    if (EnforcedWord.Word.ToLower() == Input.ToLower())
+                    {
+                        target = EnforcedWord;
+                        break;
+                    }
+                }
+                if (target != null)
+                {
+                    EnforcedWords.Remove(target);
+                }
+
+                target = new SpecificWord(Input);
+                target.NeedsPunctuation = Punctuation;
+                target.NeedsProperCase = ProperCase;
+                EnforcedWords.Add(target);
+
+                Index = -1;
+                Input = new String("");
+                Settings = new int[3];
+                SelectedWord = new String("");
+            }
+
+            ImGui.SameLine();
+
+            if (Index == -1) ImGui.BeginDisabled();
+            if (ImGui.Button("Remove Word##EnforcedWordRemove", new Vector2(ImGui.GetWindowWidth() / 2 - 8, 25)))
+            {
+                SpecificWord? target = null;
+                foreach (SpecificWord EnforcedWord in EnforcedWords)
+                {
+                    if (EnforcedWord.Word.ToLower() == Input.ToLower())
+                    {
+                        target = EnforcedWord;
+                        break;
+                    }
+                }
+                if (target != null)
+                {
+                    EnforcedWords.Remove(target);
+                }
+                Index = -1;
+                Input = new String("");
+                Settings = new int[3];
+                SelectedWord = new String("");
+            }
+            if (Index == -1) ImGui.EndDisabled();
+
+            ImGui.Spacing();
+
+            if (ImGui.BeginListBox("##EnforcedWordListBox", new Vector2(ImGui.GetWindowWidth() - 15, 340)))
+            {
+                int index = 0;
+                foreach (SpecificWord EnforcedWord in EnforcedWords)
+                {
+                    string mode = new String("");
+                    string durS = new String("");
+                    bool is_Selected = (Index == index);
+                    if (ImGui.Selectable($"{EnforcedWord.Word} - Punctuation: {EnforcedWord.NeedsPunctuation} - Proper Case: {EnforcedWord.NeedsProperCase}", ref is_Selected))
+                    {
+                        SelectedWord = EnforcedWord.Word;
+                        Index = index;
+                        Input = EnforcedWord.Word;
+                    }
+                    index++;
+                }
+                ImGui.EndListBox();
+            }
+        }
+
+    }
+}
---
+++ WoL-Plugin/Util/Types/SpecificWordWithContext.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoLightning.WoL_Plugin.Util.Types
+{
+    // todo: this naming is kinda stupid, maybe refactor
+    public class SpecificWordWithContext : SpecificWord
+    {
+        public string MatchPreWord = "";
+        public string MatchPostWord = "";
+        public bool NeedsDirectLinking = false;
+
+        public SpecificWordWithContext(string Word) : base(Word)
+        {
+        }
+
+
+
+        public bool MatchesContext(string EntireText)
+        {
+            string EntireText_ = EntireText;
+            string MatchPreWord_ = MatchPreWord;
+            string MatchPostWord_ = MatchPostWord;
+            string Word_ = Word;
+
+            if (!NeedsProperCase)
+            {
+                EntireText_ = EntireText_.ToLower();
+                MatchPreWord_ = MatchPreWord_.ToLower();
+                MatchPostWord_ = MatchPostWord_.ToLower();
+                //Word_ = Word_.ToLower();
+            }
+
+            int preIndex = -2;
+            int postIndex = -2;
+
+            if (MatchPreWord_.Length > 0)
+            {
+                preIndex = EntireText_.IndexOf(MatchPreWord_);
+                if (preIndex == -1) return false;
+            }
+
+            if (MatchPostWord_.Length > 0)
+            {
+                postIndex = EntireText_.IndexOf(MatchPostWord_);
+                if (postIndex == -1) return false;
+            }
+
+            if (preIndex != -2 && postIndex != -2 && preIndex >= postIndex) return false;
+
+            if (NeedsDirectLinking)
+            {
+                // todo
+            }
+
+            return true;
+        }
+
+        public bool IsContextFulfilled(string EntireText)
+        {
+
+            string WordCompound = "";
+
+            if (MatchPreWord.Length > 0)
+                WordCompound += MatchPreWord;
+
+            WordCompound += Word;
+
+            if (MatchPostWord.Length > 0)
+                WordCompound += MatchPostWord;
+
+            if (!NeedsPunctuation) return EntireText.Contains(WordCompound);
+
+            return EntireText.Equals(WordCompound);
+        }
+
+        public override String ToString()
+        {
+            return MatchPreWord + " " + Word + " " + MatchPostWord;
+        }
+    }
+}
