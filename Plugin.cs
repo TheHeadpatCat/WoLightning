06633f4ccd1409b2638e4b585c29b970ecc45590
TheHeadpatCat <None.>
2026-01-01 19:17:07 +0100

    bump version

--- Plugin.cs
+++ Plugin.cs
@@ -39,7 +39,7 @@ public sealed class Plugin : IDalamudPlugin
     private const string OpenShockRemote = "/wolremote";
     private const string SwapPreset = "/wolpreset";
 
-    public static readonly Version CurrentVersion = new(6, 1, 1, 'b');
+    public static readonly Version CurrentVersion = new(6, 2, 0, 'b');
 
     public const string randomKey = "Currently Unused";
 
